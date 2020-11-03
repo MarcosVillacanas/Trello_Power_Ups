6c6019b0e7fbac14476f6f7d8b778c0f17c9faa2
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-11-03 11:14:44 +0100

    test 4

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,6 +1,6 @@
 let getBadges = function(t){
-    return t.card('Test')
-        .get('Test')
+    return t.card('name')
+        .get('name')
         .then(function(cardName){
             console.log('We just loaded the card name for fun: ' + cardName);
         });
