04c0b5ed004b488b4af6cdf59cf9308f6d7cfc9f
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-11-03 12:36:40 +0100

    test 19, check description set badge

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,20 +2,33 @@ const CORRECT_ICON = 'src/icon/correct.svg';
 
 
 let setBadges = function(t, flag){
-    console.log(flag);
-    if (flag) {
-        return {
-            text: 'Approved',
-            icon: CORRECT_ICON,
-            color: 'green'
-        }
-    } else {
-        return {
-            text: 'Invalid',
-            icon: CORRECT_ICON,
-            color: 'red'
-        }
-    }
+    return t.card('name')
+        .get('name')
+        .then(function(cardName){
+            console.log('We just loaded the card name for fun: ' + cardName);
+            return [{
+                // Dynamic badges can have their function rerun
+                // after a set number of seconds defined by refresh.
+                // Minimum of 10 seconds.
+                dynamic: function(){
+                    // we could also return a Promise that resolves to
+                    // this as well if we needed to do something async first
+                    return {
+                        text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
+                        icon: './images/icon.svg',
+                        color: 'green',
+                        refresh: 10 // in seconds
+                    };
+                }
+            }, {
+                // It's best to use static badges unless you need your
+                // badges to refresh.
+                // You can mix and match between static and dynamic
+                text: 'Static',
+                icon: CORRECT_ICON, // for card front badges only
+                color: null
+            }];
+        });
 }
 
 let checkDesc = function(t){
