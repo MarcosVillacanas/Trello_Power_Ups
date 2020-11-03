e4c0d532d08ac8627e0512c31184dad434b45c5e
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-11-03 10:46:23 +0100

    test

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,10 +1,3 @@
-console.log('Hello World!');
-
-/*
-Trello available data:
-https://developer.atlassian.com/cloud/trello/power-ups/client-library/accessing-trello-data/
-*/
-
 TrelloPowerUp.initialize({
     'card-badges': function(t, opts) {
         return t.card('all')
@@ -16,3 +9,11 @@ TrelloPowerUp.initialize({
             })
     }
 });
+
+console.log('Hello World!');
+
+/*
+Trello available data:
+https://developer.atlassian.com/cloud/trello/power-ups/client-library/accessing-trello-data/
+*/
+
