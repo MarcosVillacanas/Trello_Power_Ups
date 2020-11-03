70afc4ee3fa052bf3067920071e8fc21c9a5512d
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-11-03 10:17:38 +0100

    Load A Single Card

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,7 +1,17 @@
 console.log('Hello World!');
 
+/*
+Trello available data:
+https://developer.atlassian.com/cloud/trello/power-ups/client-library/accessing-trello-data/
+*/
+
 window.TrelloPowerUp.initialize({
     'card-badges': function(t, ops) {
-        return [];
+        return t.card('all').then(function(card) {
+            console.log(card);
+            return[{
+                text: card.idShort
+            }];
+        });
     }
 });
