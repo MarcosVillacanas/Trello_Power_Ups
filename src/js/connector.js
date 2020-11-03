585b74669681cc41195413c2dc6d60e149b7299e
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-11-03 10:34:57 +0100

    window might be no longer needed

--- src/js/connector.js
+++ src/js/connector.js
@@ -5,13 +5,14 @@ Trello available data:
 https://developer.atlassian.com/cloud/trello/power-ups/client-library/accessing-trello-data/
 */
 
-window.TrelloPowerUp.initialize({
-    'card-badges': function(t, ops) {
-        return t.card('all').then(function(card) {
-            console.log(card);
-            return[{
-                text: card.idShort
-            }];
-        })
+TrelloPowerUp.initialize({
+    'card-badges': function(t, opts) {
+        return t.card('all')
+            .then(function(card) {
+                console.log(card);
+                return[{
+                    text: card.idShort
+                }];
+            })
     }
 });
