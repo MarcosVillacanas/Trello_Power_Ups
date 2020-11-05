c01e801eb3e1216e6e1aebdb582b288c5bbb38b4
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-11-05 17:54:11 +0100

    test 127, api

--- js/connector.js
+++ js/connector.js
@@ -1,3 +1,5 @@
+require("babel-polyfill")
+
 const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 const WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
 const BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
