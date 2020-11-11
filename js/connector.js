36fac34d5438336c4deee2dd77b9bf6e1b7d97d3
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-11-11 10:12:14 +0100

    test 166, api

--- js/connector.js
+++ js/connector.js
@@ -1,5 +1,6 @@
 require('babel-polyfill');
 require('babel-plugin-transform-runtime');
+require('bluebird');
 
 const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 const WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
