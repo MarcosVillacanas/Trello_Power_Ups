8461466a228f13b0630a84bfbe711a57a1fe6303
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-11-11 10:15:32 +0100

    test 167, api

--- js/connector.js
+++ js/connector.js
@@ -1,6 +1,6 @@
 require('babel-polyfill');
 require('babel-plugin-transform-runtime');
-require('bluebird');
+const Promise = require('bluebird');
 
 const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 const WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
@@ -155,7 +155,6 @@ async function getAboveCards(okrList, API_KEY, TOKEN) {
             + okrList + '/cards?key=' + API_KEY + '&token=' + TOKEN, {
             method: 'GET'
         });
-        const Promise = window.TrelloPowerUp.Promise;
         const cards = Promise.resolve(response.json());
         console.log(cards);
 
