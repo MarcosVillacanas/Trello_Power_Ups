5f53efa740ee3044599d6b41e50e5918bbca954c
MarcosVillacanas <marcosvillacanas@icloud.com>
2020-11-11 12:00:21 +0100

    test 183, api

--- js/connector.js
+++ js/connector.js
@@ -4,7 +4,7 @@ require('babel-plugin-transform-runtime');
 const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 const WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
 const BLACK_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-black.svg';
-const ROCKET_ICON = 'rocket.png';
+const ROCKET_ICON = 'rocket.svg';
 
 const API_KEY = '5b78ab18393c29272dc25f6772ae72bf';
 
